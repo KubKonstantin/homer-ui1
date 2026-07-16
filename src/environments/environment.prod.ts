367f98263011a72a168c9f7481277e90a82f7b00
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2026-07-17 00:49:33 +0600

    Include RTCP receiver reports in QoS metrics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,16 @@ import { VERSION } from '../VERSION';
 
 declare const self: any;
 
+function getRtWatcherServer(config: any): string {
+  return config?.rtwatcher_config?.host || config?.RTWATCHER_SERVER || config?.RTWATCHER_HOST || location.protocol + '//' + location.host;
+}
+
 let _environment: any = {
   production: true,
   environment: VERSION,
   isHomerAPI: true,
   apiUrl: location.protocol + '//' + (location.host) + '/api/v3',
+  rtWatcherServer: getRtWatcherServer(self?.GLOBAL_CONFIG),
 };
 
 if (typeof self?.GLOBAL_CONFIG == "object") {
@@ -16,6 +21,7 @@ if (typeof self?.GLOBAL_CONFIG == "object") {
   } else if (PREFIX) {
       _environment.apiUrl = location.protocol + '//' + (location.host) + PREFIX + 'api/v3';
   }
+  _environment.rtWatcherServer = getRtWatcherServer(self?.GLOBAL_CONFIG);
 }
 
 export const environment = _environment;
